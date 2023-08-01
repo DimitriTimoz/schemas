6f2cd5aaca4c7b695debc96228e650cd982275fe
Dimitri <dimitri.timoz@orange.fr>
2023-08-01 15:35:50 +0200

    fix syntax

--- src/trait.rs
+++ src/trait.rs
@@ -3,7 +3,7 @@ use crate::prelude::*;
 pub trait Schema {
     fn new() -> Self;
     fn add_property(&mut self, name: String, value: String) -> Result<(), >;
-    fn add_item(&mut self, name: String, item: Types) -> Result<(), Error;
+    fn add_item(&mut self, name: String, item: Types) -> Result<(), Error>;
 }
 
 pub enum Error {
