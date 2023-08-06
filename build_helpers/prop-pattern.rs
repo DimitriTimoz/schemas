c7b6543ce1ff847fd4c28e5cdf40d6661eb0bb00
Mubelotix <mubelotix@gmail.com>
2023-08-06 13:26:08 +0200

    Add conversion

--- build_helpers/prop-pattern.rs
+++ build_helpers/prop-pattern.rs
@@ -3,3 +3,6 @@
 pub enum PatternTypeProp {
     PatternVariant(PatternVariant),
 }
+
+#[automatically_derived]
+impl From<PatternVariant> for PatternTypeProp { fn from(value: PatternVariant) -> Self { Self::PatternVariant(value) } }
