c0388f5f9d119575ebc1ed1164da6c6d98ee790f
Mubelotix <mubelotix@gmail.com>
2023-08-06 13:21:15 +0200

    Add helper methods

--- build_helpers/type-pattern.rs
+++ build_helpers/type-pattern.rs
@@ -7,6 +7,21 @@ pub struct PatternType {
 
 #[automatically_derived]
 impl Schema for PatternType {
+    fn add_pattern_property(&mut self, value: impl Into<PatternPropertyProp>) { self.pattern_property.push(value.into()) }
+
+    fn take_pattern_property(&mut self) -> Vec<PatternPropertyProp> { std::mem::take(&mut self.pattern_property) }
+
+    fn set_pattern_property(&mut self, value: impl Into<PatternPropertyProp>) { self.pattern_property = vec![value.into()]; }
+
+    fn set_pattern_property_vec(&mut self, value: Vec<PatternPropertyProp>) { self.pattern_property = value; }
+
+    fn clear_pattern_property(&mut self) { self.pattern_property.clear(); }
+
+    fn add_pattern_parent(&mut self, value: impl Into<PatternParent>) { self.pattern_parent.add_pattern_parent(value); }
+}
+
+#[automatically_derived]
+impl Schema for PatternType {
     fn new() -> Self {
         Self::default()
     }
